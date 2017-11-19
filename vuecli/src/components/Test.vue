978f84d937eb7c36dca72aa8ac38454ae1947ad3
xiecheng <13136666328@163.com>
2017-11-19 10:11:56 +0800

    vue-cli

---
+++ vuecli/src/components/Test.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1 class="test">{{msg}}</h1>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            msg: '测试vue-router'
+        }
+    }
+}
+</script>
+
+<style scoped>
+.test{
+    background: #ff0000;
+}
+</style>
+
+
